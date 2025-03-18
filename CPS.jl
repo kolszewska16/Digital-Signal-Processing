06341af26071069b8aecd2739c7032524d71b7c3
kolszewska16 <kingaolszewska16@gmail.com>
2025-03-18 22:37:41 +0100

    CPS

--- CPS.jl
+++ CPS.jl
@@ -12,7 +12,7 @@ const author = Dict(
 # Parametry sygnałów                                                          #
 ###############################################################################
 mean(x::AbstractVector)::Number = missing
-peak2peak(x::AbstractVector)::Real = missing
+peak2peak(x::AbstractVector)::Real = maximum(x) - minimum(x)
 energy(x::AbstractVector)::Real = missing
 power(x::AbstractVector)::Real = missing
 rms(x::AbstractVector)::Real = missing
@@ -34,15 +34,15 @@ end
 ###############################################################################
 # Modelowanie sygnałów                                                        #
 ###############################################################################
-ci_rectangular(t::Real; T::Real=1.0)::Real = missing
-ci_triangle(t::Real; T::Real=1.0)::Real = missing
-ci_literka_M(t::Real; T=1.0)::Real = missing
-ci_literka_U(t::Real; T=1.0)::Real = missing
+ci_rectangular(t::Real; T::Real=1.0)::Real = (abs(t) <= T / 2) ? 1.0 : 0.0
+ci_triangle(t::Real; T::Real=1.0)::Real = (abs(t) < T) ? (1 - abs(t)) : 0.0
+ci_literka_M(t::Real; T=1.0)::Real = (abs(t) <= T / 2) ? (abs(t) + 0.5) : 0.0
+ci_literka_U(t::Real; T=1.0)::Real = (abs(t) <= T / 2) ? (4 * t^2) : 0.0
 
 ramp_wave(t::Real)::Real = missing
 sawtooth_wave(t::Real)::Real = missing
 triangular_wave(t::Real)::Real = missing
-square_wave(t::Real)::Real = missing
+square_wave(t::Real)::Real = sign(sin(2  * π * 1 * t))
 pulse_wave(t::Real, ρ::Real=0.2)::Real = missing
 
 function impuse_repeater(g::Function, t0::Real, t1::Real)::Function 
@@ -97,5 +97,26 @@ end
 function kaiser(N; β=0, K=20)
     missing
 end
+
+end
+
+
+###############################################################################
+# Próbkowanie i kwantyzacja                                                   #
+###############################################################################
+
+quantize(L::AbstractVector)::Function = missing
+SQNR(N::Integer)::Real = missing
+SNR(Psignal::Real, Pnoise::Real)::Real = missing
+
+
+function interpolate(
+    m::AbstractVector,
+    s::AbstractVector,
+    kernel::Function=sinc
+)::Function
+    missing
+end
+
 
 end
