52ce57c513fb50bf799f4ad589aaba9173537a88
kolszewska16 <kingaolszewska16@gmail.com>
2025-04-09 19:05:08 +0200

    8.10 and 8.11

--- lab8.jl
+++ lab8.jl
@@ -39,3 +39,70 @@ end
 # fftfreq(N::Integer, fs::Real) = [(k * fs) / N for k in 0:(N - 1)]
 fftfreq(8192, 1000)
 
+# problem 8.10
+rect(N::Integer) = (N > 0) ? ones(N) : 0.0
+function dft(x::Vector)::Vector
+    N = length(x)
+    X = []
+    for k = 0:(N - 1)
+        sum_k = 0
+        for n in 0:(N - 1)
+            sum_k += x[n + 1] * exp(-im * (2 * π / N) * k * n)
+        end
+        append!(X, sum_k)
+    end
+    return X
+end
+
+function amplitude_spectrum(x::Vector, w::Vector=CPS.rect(length(x)))::Vector
+    x_win = x .* w
+    X = dft(x_win)
+    N = length(X)
+
+    A = [abs(X[i + 1]) for i in 0:(N - 1)]
+    return A
+end
+
+# amplitude_spectrum(x::Vector, w::Vector=rect(length(x)))::Vector = [abs(dft(x .* w)[i]) for i in 1:length(x)]
+using CairoMakie
+a = [0, -3, 0, 3]
+N = length(a)
+x = 0:(N - 1)
+y = amplitude_spectrum(a)
+stem(x, y)
+
+# problem 8.11
+rect(N::Integer) = (N > 0) ? ones(N) : 0.0
+function dft(x::Vector)::Vector
+    N = length(x)
+    X = []
+    for k = 0:(N - 1)
+        sum_k = 0
+        for n in 0:(N - 1)
+            sum_k += x[n + 1] * exp(-im * (2 * π / N) * k * n)
+        end
+        append!(X, sum_k)
+    end
+    return X
+end
+
+function power_spectrum(x::Vector, w::Vector=rect(length(x)))::Vector
+    x_win = x .* w
+    N = length(x)
+    X = dft(x_win)
+    P = []
+    
+    for i = 0:(N - 1)
+        pow = abs(X[i + 1])^2
+        append!(P, pow)
+    end
+    return P
+end
+
+# power_spectrum(x::Vector, w::Vector=rect(length(x)))::Vector = [abs(dft(x .* w)[i])^2 for i in 1:length(x)]
+using CairoMakie
+a = [0, -3, 0, 3]
+N = length(a)
+x = 0:(N - 1)
+y = power_spectrum(a)
+stem(x, y)
